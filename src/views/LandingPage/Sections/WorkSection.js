5fa47a8ebd3b69b3836aad5c902f85638598e513
Jose Calvano <josecalvano1546@gmail.com>
2021-11-03 19:35:03 -0300

    prueba

--- src/views/LandingPage/Sections/WorkSection.js
+++ src/views/LandingPage/Sections/WorkSection.js
@@ -24,8 +24,7 @@ export default function WorkSection() {
           <h4 className={classes.description}>
             Contacta conmigo, respnderé a la brevedad.
           </h4>
-          <form name="contact" method="POST" data-netlify="true">
-            <input type="hidden" name="form-name" value="contact" />
+          <form method="POST" data-netlify="true">
             <GridContainer>
               <GridItem xs={12} sm={12} md={6}>
                 <CustomInput
@@ -64,6 +63,36 @@ export default function WorkSection() {
                 </Button>
               </GridItem>
             </GridContainer>
+          </form>
+          <form name="contact" method="POST" data-netlify="true">
+            <input type="hidden" name="form-name" value="contact" />
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Role:{" "}
+                <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select>
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message"></textarea>
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
           </form>
         </GridItem>
       </GridContainer>
